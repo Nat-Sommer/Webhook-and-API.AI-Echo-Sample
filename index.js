9016b8fa3b5743fc377aa2f7e6cb33fe6ed4cd28
Nat-Sommer <61732273+Nat-Sommer@users.noreply.github.com>
2020-03-03 10:53:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,29 +12,6 @@ restService.use(
 );
 
 restService.use(bodyParser.json());
-
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  
-  var speechResponse = {
-    google: {
-      expectUserResponse: true,
-      richResponse: {
-        items: [
-          {
-            simpleResponse: {
-              textToSpeech: speech
-            }
-          }
-        ]
-      }
-    }
-  };
   
   return res.json({
     payload: speechResponse,
